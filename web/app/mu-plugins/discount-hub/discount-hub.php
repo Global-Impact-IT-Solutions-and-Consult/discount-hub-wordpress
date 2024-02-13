d2e1c15f3f3b9587f1b3c5811e4cca2ac1409831
Olaolu Johnson <johnsonolaolu@gmail.com>
2024-02-13 17:39:17 +0100

    fix: updated

--- web/app/mu-plugins/discount-hub/discount-hub.php
+++ web/app/mu-plugins/discount-hub/discount-hub.php
@@ -27,6 +27,12 @@ function create_posttype()
             'public' => true,
             'has_archive' => true,
             'show_in_rest' => true,
+            'hierarchical' => true, # set to false if you don't want parent/child relationships for the entries
+            'show_in_graphql' => true, # Set to false if you want to exclude this type from the GraphQL Schema
+            'graphql_single_name' => 'product',
+            'graphql_plural_name' => 'products', # If set to the same name as graphql_single_name, the field name will default to `all${graphql_single_name}`, i.e. `allDocument`.
+            'public' => true, # set to false if entries of the post_type should not have public URIs per entry
+            'publicly_queryable' => true, # Set to false if entries should only be queryable in WPGraphQL by authenticated requests
             'rewrite' => array('slug' => 'product'),
             'taxonomies' => array('category', 'post_tag')
         )
@@ -45,6 +51,12 @@ function create_posttype()
             'public' => true,
             'has_archive' => true,
             'show_in_rest' => true,
+            'hierarchical' => true, # set to false if you don't want parent/child relationships for the entries
+            'show_in_graphql' => true, # Set to false if you want to exclude this type from the GraphQL Schema
+            'graphql_single_name' => 'shop',
+            'graphql_plural_name' => 'shops', # If set to the same name as graphql_single_name, the field name will default to `all${graphql_single_name}`, i.e. `allDocument`.
+            'public' => true, # set to false if entries of the post_type should not have public URIs per entry
+            'publicly_queryable' => true, # Set to false if entries should only be queryable in WPGraphQL by authenticated requests
             'rewrite' => array('slug' => 'shop'),
             'taxonomies' => array('category')
         )
